614ea9951ae5584f2f221b2ce3c15db10f4e4445
OnkelDagobert <onkeldagobert1986@googlemail.com>
2013-01-16 23:35:03 +0100

    Start Script

---
+++ start_Last_Resistance(Win7_64bit).bat
@@ -0,0 +1,3 @@
+set ns2path=""
+for /f "tokens=2*" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 4920" /v "InstallLocation"') do set "ns2path=%%b"
+Start "Last Resistance MOD" /D "%ns2path%" "%ns2path%\ns2.exe" -game "%cd%" -hotload
